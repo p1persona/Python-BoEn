66458f5677224f1e15df0d2f4fed17455c692455
ZEHRA <p1bypersona@gmail.com>
2026-09-08 13:45:16 +0300

    Add files via upload

---
+++ temel bileşenler.py
@@ -0,0 +1,63 @@
+# çıktı alma
+
+isim = "persona"
+print(isim)
+
+
+
+
+# veri tipi sorgulama
+
+isim  = "persona"
+isim1 = 7
+print(type(isim1))
+
+
+
+
+# alıştırma
+
+isim = "p1"
+d_yili = 2000
+yil = 2026
+yas_hesapla = yil - d_yili
+
+print(isim, "'in", "doğum yılı", d_yili, "yaşı", yas_hesapla)
+print(isim + "'in doğum yılı" + str(d_yili) + "yaşı" + str(yas_hesapla))
+
+
+
+
+# str'i int'a çevirme
+
+yas = "30"
+print(type(yas))
+
+yas = int("30")
+print(type(yas))
+
+sayi = float(4000)
+print(sayi)
+
+
+
+
+# str veri tipi sözel ifadeler
+
+takim = "galatasaray"
+takim1 = 'besiktas'
+
+#hikaye = 'zehra'nın en sevdiği..'        (hata)        alt gr + ?  kullanılmalı (\)
+
+
+
+
+# str verileri toplama
+
+print(takim + takim1)
+print(takim * 5)
+
+
+
+
+ 
