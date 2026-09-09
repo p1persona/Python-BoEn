21ea1ce2992f00f53a3b90e0010b040b6413dc57
Clary Zehra <p1bypersona@gmail.com>
2026-09-09 14:01:03 +0300

    Add files via upload

---
+++ demetler.py
@@ -0,0 +1,12 @@
+
+# DEMETLER
+
+deneme = ("zehra","uyar","person")
+
+print(type(deneme))
+print(deneme[-1])
+
+# deneme[0] = "p0"         |           güncellenemez
+
+
+demet = ("1.veri","2.veri","3.veri")
