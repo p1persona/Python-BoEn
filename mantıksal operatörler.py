b67e97df99e42185c146073166a6a68b2948de0a
Clary Zehra <p1bypersona@gmail.com>
2026-09-12 00:02:10 +0300

    Add files via upload

---
+++ mantıksal operatörler.py
@@ -0,0 +1,23 @@
+
+# MANTIKSAL OPERATÖRLERİ
+
+deneme = 8<10 and 11<12
+print(deneme)
+
+
+
+db_ps = 1234
+db_ka = "zehra"
+kullanici_adi = input("adınızı girin:")
+kullanici_sifre = int(input("şifrenizi girin:"))
+kontrol = db_ps == kullanici_sifre and db_ka == kullanici_adi
+print(kontrol)
+
+
+
+db_ps1 = 1234
+db_ka1 = "zehra"
+kullanici_adi1 = input("adınızı girin:")
+kullanici_sifre1 = int(input("şifrenizi girin:"))
+kontrol1 = db_ps1 == kullanici_sifre1 or db_ka1 == kullanici_adi1
+print(kontrol1)
