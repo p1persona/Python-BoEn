8dabc598ff832c920a8cc4879e148b0db6626526
Clary Zehra <p1bypersona@gmail.com>
2026-09-13 12:40:59 +0300

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,12 @@
+
+# ELIF KOŞULU
+
+sayi = 5
+if sayi == 5:
+    print("sayınız eşittir=",sayi)
+
+elif sayi == 6:
+    print("sayınız eşittir =", sayi)
+
+else:
+    print("sayınız eşit değildir =",sayi)
