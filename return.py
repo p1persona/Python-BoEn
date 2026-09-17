43f52d4f080f6ced054f1526db858f078a5286d1
Clary Zehra <p1bypersona@gmail.com>
2026-09-18 02:27:23 +0300

    Add files via upload

---
+++ return.py
@@ -0,0 +1,15 @@
+
+# RETURN KOMUTU
+
+def toplama(x,y):
+    toplam=x+y
+    #print(toplam)
+    return x+y
+
+
+a=toplama(5,5)
+b=5
+
+print(a+b)
+
+# DIŞ DÜNYADA FONKSİYONDAKİ DEĞERİ KULLANMAK İÇİN KULLANILIR.
