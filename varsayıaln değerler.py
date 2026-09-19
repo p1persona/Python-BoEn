a1ce2830cec33d905839c799fe861174f0387a00
Clary Zehra <p1bypersona@gmail.com>
2026-09-19 13:47:57 +0300

    Add files via upload

---
+++ varsayıaln değerler.py
@@ -0,0 +1,23 @@
+
+# VARSAYILAN DEĞERLER
+
+def kullanici(isim,soyisim,depertman):
+    print(f"isminiz:{isim}\nsoyisminiz:{soyisim}\ndeperman:{depertman}")
+
+
+kullanici("zehra","uyar","yazılım")
+
+
+
+
+
+def kullanici(isim,soyisim,depertman="bu alan boş bırakıldı"):
+    print(f"isminiz:{isim}\nsoyisminiz:{soyisim}\ndeperman:{depertman}")
+
+
+kullanici("zehra","uyar")
+
+
+
+
+
